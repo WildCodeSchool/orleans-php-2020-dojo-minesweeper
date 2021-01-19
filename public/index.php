a087928c3bebe25b9bb80eca00db71abe58372b4
Sylvain Blondeau <sylvain.blondeau@wildcodeschool.com>
2021-01-19 13:28:28 +0100

    minesweeper

--- public/index.php
+++ public/index.php
@@ -1,37 +1,36 @@
 <?php
 
-require '../vendor/autoload.php';
-
-session_start();
-
 use App\MineSweeper;
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 
-$tiles = [
-    [0,MineSweeper::BOMB,0],
-    [MineSweeper::BOMB,0,0],
-    [MineSweeper::BOMB,MineSweeper::BOMB,0],
-];
+require '../vendor/autoload.php';
 
-if (isset($_GET['reset'])) {
-    unset($_SESSION['minesweeper']);  
+session_start();
+
+$tiles = [
+    [0,MineSweeper::BOMB,0, 0],
+    [MineSweeper::BOMB,0,0 ,0],
+    [MineSweeper::BOMB,MineSweeper::BOMB,0, 0],
+];
+if(isset($_GET['reset'])) {
+    unset($_SESSION['minesweeper']);
 }
 
 $mineSweeper = $_SESSION['minesweeper'] ?? new MineSweeper($tiles);
 
-if (isset($_GET['x']) && isset($_GET['y'])) {
+
+if(isset($_GET['x']) && isset($_GET['y'])) {
     try {
         $mineSweeper->play($_GET['x'], $_GET['y']);
-    } catch (Exception $exception) {
+    } catch(Exception $exception) {
         $error = $exception->getMessage();
-    } 
-    
+    }
 }
 
 $_SESSION['minesweeper'] = $mineSweeper;
 
-$loader = new FilesystemLoader(__DIR__.'/../src/view');
+$loader = new FilesystemLoader(__DIR__ . '/../src/View');
 $twig = new Environment($loader, []);
 
 echo $twig->render('index.html.twig', [
